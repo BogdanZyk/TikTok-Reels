9efee633ccf5262ad4d32cdc909cf16e25125a58
Bogdan Zykov <b.zykov@brightlab.me>
2022-08-31 00:47:49 +0300

    Initial Commit

---
+++ Shared/TikTok_ReelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_ReelsApp.swift
+//  Shared
+//
+//  Created by Bogdan Zykov on 31.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_ReelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
